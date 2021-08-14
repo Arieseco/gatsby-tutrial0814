f52724f228028918e7cdada844a7627b00d96417
Hiroaki Ikeda <fusianasan.jimso@gmail.com>
2021-08-14 22:33:48 +0900

    Finished Gatsby Tutorial Part 2

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
+import Layout from '../components/layout'
 
-const about = () => {
+const AboutPage = () => {
   return (
-    <div>
-      
-    </div>
+    <Layout pageTitle="About Me">
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </Layout>
   )
 }
 
-export default about
+export default AboutPage
